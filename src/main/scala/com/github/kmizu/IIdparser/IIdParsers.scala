61f0f16d0a3e10b07ac546aba9314f9da967ac3c
Kota Mizushima <mizukota@gmail.com>
2016-09-27 00:56:58 +0900

    Refactor

--- src/main/scala/com/github/kmizu/IIdparser/IIdParsers.scala
+++ src/main/scala/com/github/kmizu/IIdparser/IIdParsers.scala
@@ -19,75 +19,67 @@ abstract class IIdParsers {
 
   abstract class Parser[+T] {
     def apply(input: Point): ParseResult[T]
-    def /[U >: T](other: Parser[U]): Parser[U] = new Parser[U] {
-      def apply(input: Point): ParseResult[U] = {
-        this(input) match {
-          case success@ParseSuccess(_, _) => success
-          case ParseFailure => other(input)
-        }
+    def /[U >: T](other: Parser[U]): Parser[U] = parserOf{input =>
+      this(input) match {
+        case success@ParseSuccess(_, _) => success
+        case ParseFailure => other(input)
       }
     }
-    def ~[U](other: Parser[U]): Parser[T ~ U] = new Parser[T ~ U] {
-      def apply(input: Point): ParseResult[T ~ U] = {
-        this(input) match {
-          case success1@ParseSuccess(_, _) =>
-            val next = success1.next
-            other(next) match {
-              case success2@ParseSuccess(_, _) =>
-                ParseSuccess(new ~(success1.value, success2.value), success2.next)
-              case ParseFailure => ParseFailure
-            }
-          case ParseFailure => ParseFailure
-        }
+    def ~[U](other: Parser[U]): Parser[T ~ U] = parserOf{input =>
+      this(input) match {
+        case success1@ParseSuccess(_, _) =>
+          val next = success1.next
+          other(next) match {
+            case success2@ParseSuccess(_, _) =>
+              ParseSuccess(new ~(success1.value, success2.value), success2.next)
+            case ParseFailure => ParseFailure
+          }
+        case ParseFailure => ParseFailure
       }
     }
   }
 
-  def up(content: String): Parser[String] = new Parser[String] {
-    def apply(input: Point): ParseResult[String] = {
-      val newX = input.x
-      val newY = input.y - 1
-      if(inRange(newX, newY) && mapping(newX)(newY) == content) {
-        ParseSuccess(mapping(newX)(newY), Point(newX, newY))
-      }else {
-        ParseFailure
-      }
+  def parserOf[T](block: Point => ParseResult[T]): Parser[T] = new Parser[T] {
+    def apply(input: Point): ParseResult[T] = block(input)
+  }
+
+  def up(content: String): Parser[String] = parserOf{input =>
+    val newX = input.x
+    val newY = input.y - 1
+    if(inRange(newX, newY) && mapping(newX)(newY) == content) {
+      ParseSuccess(mapping(newX)(newY), Point(newX, newY))
+    }else {
+      ParseFailure
     }
   }
 
-  def down(content: String): Parser[String] = new Parser[String] {
-    def apply(input: Point): ParseResult[String] = {
-      val newX = input.x
-      val newY = input.y + 1
-      if(inRange(newX, newY) && mapping(newX)(newY) == content) {
-        ParseSuccess(mapping(newX)(newY), Point(newX, newY))
-      }else {
-        ParseFailure
-      }
+  def down(content: String): Parser[String] = parserOf{input =>
+    val newX = input.x
+    val newY = input.y + 1
+    if(inRange(newX, newY) && mapping(newX)(newY) == content) {
+      ParseSuccess(mapping(newX)(newY), Point(newX, newY))
+    }else {
+      ParseFailure
     }
   }
 
-  def right(content: String): Parser[String] = new Parser[String] {
-    def apply(input: Point): ParseResult[String] = {
-      val newX = input.x + 1
-      val newY = input.y
-      if(inRange(newX, newY) && mapping(newX)(newY) == content) {
-        ParseSuccess(mapping(newX)(newY), Point(newX, newY))
-      }else {
-        ParseFailure
-      }
+  def right(content: String): Parser[String] = parserOf{input =>
+    val newX = input.x + 1
+    val newY = input.y
+    if(inRange(newX, newY) && mapping(newX)(newY) == content) {
+      ParseSuccess(mapping(newX)(newY), Point(newX, newY))
+    }else {
+      ParseFailure
     }
   }
 
-  def left(content: String): Parser[String] = new Parser[String] {
-    def apply(input: Point): ParseResult[String] = {
-      val newX = input.x - 1
-      val newY = input.y
-      if(inRange(newX, newY) && mapping(newX)(newY) == content) {
-        ParseSuccess(mapping(newX)(newY), Point(newX, newY))
-      }else {
-        ParseFailure
-      }
+  def left(content: String): Parser[String] = parserOf{input =>
+    val newX = input.x - 1
+    val newY = input.y
+    if(inRange(newX, newY) && mapping(newX)(newY) == content) {
+      ParseSuccess(mapping(newX)(newY), Point(newX, newY))
+    }else {
+      ParseFailure
     }
   }
 }
